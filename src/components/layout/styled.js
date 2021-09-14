ec9999b048cd2ace44074b61495e41c4be82c377
PedroIvo00 <pedroivo00@hotmail.com>
2021-09-14 13:06:36 -0300

    Initial commit

---
+++ src/components/layout/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const WrapperLayout = styled.section`
+  margin: 16px;
+  background-color: #fcfcfc;
+  border-radius: 15px;
+  border: 1px solid #ccc;
+`;
